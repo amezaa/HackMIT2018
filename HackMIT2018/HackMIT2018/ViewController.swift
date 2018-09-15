933f30aa9f7a21b33ecc84f8cadeba759f0ef3d7
Adrian Meza <bowenite@Adrians-MacBook-Pro.local>
2018-09-15 15:21:23 -0400

    basic iOS Template

---
+++ HackMIT2018/HackMIT2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackMIT2018
+//
+//  Created by Adrian Meza on 9/15/18.
+//  Copyright © 2018 Adrian Meza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
